5181799c7ad117cbb7581dd53ab8f3e8f21b0240
Eric Huang <71966251+hsnu145522@users.noreply.github.com>
2022-03-31 01:01:40 +0800

    Update dfs_stack.py

--- AI_HW2/dfs_stack.py
+++ AI_HW2/dfs_stack.py
@@ -3,6 +3,8 @@ edgeFile = 'edges.csv'
 
 
 def dfs(start, end):
+    #initialize mystack(a stack implemented by list for DFS), mydict(a dictionary for recording the preceder of nodes),
+    #myset(a set to check if a node is visited)
     path = []
     dist = 0
     num_visited = 0
@@ -12,46 +14,46 @@ def dfs(start, end):
     with open(edgeFile,newline='') as csvfile:
         rows1 = csv.reader(csvfile)
         rows = []
+        #create a list rows for data in csvfile.
         for row in rows1:
             rows.append(row)
-        mystack.append(start)
-
-        while len(mystack)!=0:
+            
+        mystack.append(start)         #put the start node in the stack.
+        while len(mystack)!=0:        #DFS algorithm
             thisnode = mystack.pop()
-            if end in mydict:
+            if end in mydict:         #if end is accessable, break.
                 myset.add(end)
                 break
-            if thisnode in myset:
+            if thisnode in myset:     #if the node was visited,continue.
                 continue
             else:
-                pass
-            first = False
+                myset.add(thisnode)   #mark thisnode as visited.
+            
+            first = False             #first is for the first row of csvfile.
             for row in rows:
-                
                 if not(first):
                     first = True
                     continue
+                #put the nodes that are adjacent to thisnode into the stack.
                 if int(row[0]) == thisnode and not(int(row[1]) in myset):
                     mystack.append(int(row[1]))
                     mydict[int(row[1])] = (int(row[0]),row[2])         
-            myset.add(thisnode)
-            #print(thisnode)
-        print("Done with DFS.")
+            
+        '''
+        Last part is to trace back from the end node.
+        Record the distance in dist and the path at list path.
+        Reverse the list order.
+        record the number of visited nodes in num_visited, which is the length of myset.
+        '''
         path.append(end)
         nownode = end
         while nownode != start:
             nownode,thisdist = mydict[nownode][0],mydict[nownode][1]
             path.append(nownode)
             dist+=float(thisdist)
-        path1 = []
-        for i in path:
-            path1.insert(0,i)
+        path.reverse()
         num_visited = len(myset)
-            
-            
-        
-            
-    return path1, dist, num_visited
+    return path, dist, num_visited
 
 
 if __name__ == '__main__':
