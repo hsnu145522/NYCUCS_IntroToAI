b07c7345110f2cd252b5066937515b61f5ecb8e7
Eric Huang <71966251+hsnu145522@users.noreply.github.com>
2022-03-31 01:02:46 +0800

    Update astar.py

--- AI_HW2/astar.py
+++ AI_HW2/astar.py
@@ -5,10 +5,13 @@ heuristicFile = 'heuristic.csv'
 
 
 def astar(start, end):
+    #initialize myqueue(a priority_queue implemented by list+heapq functions for A*),
+    #mydict(a dictionary for recording the preceder of nodes),
+    #myset(a set to check if a node is visited).
     path = []
     dist = 0
     num_visited = 0
-    h_distance = {}
+    h_distance = {}      #create a dictionary h_distance for data in heurisiticFile.
     myqueue = []
     mydict = {}
     myset = set()
@@ -19,39 +22,54 @@ def astar(start, end):
             if not(first):
                 first = True
                 continue
-            h_distance[int(row[0])] = float(row[2]) #row[i] task[i] for i=1,2,3
+            h_distance[int(row[0])] = float(row[1]) #row[i] task[i] for i=1,2,3
+            
     with open(edgeFile,newline='') as csvfile:
         rows1 = csv.reader(csvfile)
         rows = []
+                #create a list rows for data in csvfile(edgeFile).
         for row in rows1:
             rows.append(row)
         
-        heappush(myqueue,(0,start))
-        while not(len(myqueue)==0):
+        heappush(myqueue,(0,start))                      #put the start node in the queue.
+        while not(len(myqueue)==0):                      #A* algorithm
             thisdist, thisnode = heappop(myqueue)
-            if thisnode != start:
-                thisdist -= h_distance[int(thisnode)]
-            if thisnode in myset:
+            if thisnode != start:                        #Since distance from start to thisnode + h_distance[thisnode]
+                thisdist -= h_distance[thisnode]         #is storded, it is necessary to minus it here.
+                
+            if thisnode in myset:                        #if the node was visited,continue.
                 continue
             else:
-                myset.add(thisnode)
-                if end in myset:
+                myset.add(thisnode)                      #mark thisnode as visited.
+                if end in myset:                         #if end is accessable, break.
                     break
-            first = False
+                    
+            first = False                                #first is for the first row of csvfile.
             for row in rows:
                 if not(first):
                     first = True
                     continue
                 if int(row[0]) == thisnode and not(int(row[1]) in myset):
+                    #put the nodes that are adjacent to thisnode into the priority_queue.
+                    #In order to let heap work properly, store information in tuples.
+                    #Store information as (distance from start to int(row[1])+h_distance[int(row[1])], int(row[1])).
                     heappush(myqueue,(float(row[2])+thisdist+h_distance[int(row[1])],int(row[1])))
                     if int(row[1]) in mydict:
                         temp = mydict[int(row[1])]
+                        #If there is a way nearier than the former one, update it.
+                        #It's ok to not delete the information in the priority queue,
+                        #since, nearier ones will be pop out earlier.
                         if temp[2]>float(row[2])+thisdist+h_distance[int(row[1])]:
                             mydict[int(row[1])] = (int(row[0]),row[2],float(row[2])+thisdist+h_distance[int(row[1])])
                     else:
                         mydict[int(row[1])] = (int(row[0]),row[2],float(row[2])+thisdist+h_distance[int(row[1])])
                 
-                    
+    '''
+    Last part is to trace back from the end node.
+    Record the distance in dist and the path at list path.
+    Reverse the list order.
+    record the number of visited nodes in num_visited, which is the length of myset.
+    '''             
     path.append(end)
     nownode = end
     while nownode != start:
